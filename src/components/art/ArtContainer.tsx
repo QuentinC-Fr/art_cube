b2a9225ec3fb78ac6acbc66922f92b6694267e5b
Quentin <quentin.codelupi@hotmail.fr>
2021-11-09 17:50:04 +0100

    ADD - Text on the cube with some random repartition

--- src/components/art/ArtContainer.tsx
+++ src/components/art/ArtContainer.tsx
@@ -1,7 +1,9 @@
 import { OrbitControls, Text } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import React from "react";
 import { shallowEqual, useSelector } from "react-redux";
 import { Euler, Vector3 } from "three";
+import { textRepartition } from "../../helpers/helperTextRepartition";
 
 const Wall = ({
   height = 200,
@@ -91,7 +93,7 @@ interface CustomTextProps {
 }
 
 const CustomText: React.FunctionComponent<CustomTextProps> = ({
-  maxWidth = 200,
+  maxWidth = 180,
   maxHeight = 200,
   children,
   color = "#fff",
@@ -100,11 +102,12 @@ const CustomText: React.FunctionComponent<CustomTextProps> = ({
 }) => {
   const lineHeight = 0.75;
   const letterSpacing = 0;
-  const fontSize = 8;
+  const fontSize = 10;
   const textAlign = "justify";
   return (
     <mesh position={position} rotation={rotation}>
       <Text
+        whiteSpace={"normal"}
         maxWidth={maxWidth}
         color={color}
         anchorX="center"
@@ -113,6 +116,7 @@ const CustomText: React.FunctionComponent<CustomTextProps> = ({
         lineHeight={lineHeight}
         letterSpacing={letterSpacing}
         textAlign={textAlign}
+        fillOpacity={1}
         font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
       >
         {children}
@@ -121,7 +125,14 @@ const CustomText: React.FunctionComponent<CustomTextProps> = ({
   );
 };
 
-const InnerTextBox = () => {
+interface CustomInnerTextBoxProps {
+  text?: string[];
+}
+const InnerTextBox: React.FunctionComponent<CustomInnerTextBoxProps> = ({
+  text = [],
+}) => {
+  const repartition = textRepartition(text, 5);
+
   return (
     <mesh>
       {/* Left */}
@@ -129,79 +140,107 @@ const InnerTextBox = () => {
         rotation={new Euler(0, Math.PI / 2, 0)}
         position={new Vector3(0, 0, 0)}
       >
-        LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD
-        TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM
-        VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA
-        COMMODO CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE
-        VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR. EXCEPTEUR SINT
-        OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT
-        MOLLIT ANIM ID EST LABORUM.
+        {repartition[0]}
       </CustomText>
       {/* Back */}
       <CustomText
         rotation={new Euler(0, 0, 0)}
         position={new Vector3(100, 0, -100)}
       >
-        LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD
-        TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM
-        VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA
-        COMMODO CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE
-        VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR. EXCEPTEUR SINT
-        OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT
-        MOLLIT ANIM ID EST LABORUM.
+        {repartition[1]}
       </CustomText>
       {/* Right */}
       <CustomText
         rotation={new Euler(0, Math.PI + Math.PI / 2, 0)}
         position={new Vector3(200, 0, 0)}
       >
-        LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD
-        TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM
-        VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA
-        COMMODO CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE
-        VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR. EXCEPTEUR SINT
-        OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT
-        MOLLIT ANIM ID EST LABORUM.
+        {repartition[2]}
       </CustomText>
       {/* Top  */}
       <CustomText
         rotation={new Euler(Math.PI / 2, 0, 0)}
         position={new Vector3(100, 100, 0)}
       >
-        LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD
-        TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM
-        VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA
-        COMMODO CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE
-        VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR. EXCEPTEUR SINT
-        OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT
-        MOLLIT ANIM ID EST LABORUM.
+        {repartition[3]}
       </CustomText>
       {/* Bot  */}
       <CustomText
         rotation={new Euler(Math.PI + Math.PI / 2, 0, 0)}
         position={new Vector3(100, -100, 0)}
       >
-        1391 characters max
+        {repartition[4]}
       </CustomText>
     </mesh>
   );
 };
 
-function Cube() {
+interface CustomOuterTextBoxProps {
+  text?: string[];
+}
+const OuterTextBox: React.FunctionComponent<CustomOuterTextBoxProps> = ({
+  text = [],
+}) => {
+  const repartition = textRepartition(text, 4);
+  console.log(repartition[0], repartition[0].length);
+  return (
+    <mesh>
+      {/* Left */}
+      <CustomText
+        rotation={new Euler(0, Math.PI + Math.PI / 2, 0)}
+        position={new Vector3(-0.2, 0, 0)}
+      >
+        {repartition[0]}
+      </CustomText>
+      {/* Right */}
+      <CustomText
+        rotation={new Euler(0, Math.PI / 2, 0)}
+        position={new Vector3(200.2, 0, 0)}
+      >
+        {repartition[1]}
+      </CustomText>
+      {/* Top  */}
+      <CustomText
+        rotation={new Euler(Math.PI + Math.PI / 2, 0, 0)}
+        position={new Vector3(100, 100.2, 0)}
+      >
+        {repartition[2]}
+      </CustomText>
+      {/* Bot  */}
+      <CustomText
+        rotation={new Euler(Math.PI / 2, 0, 0)}
+        position={new Vector3(100, -100.2, 0)}
+      >
+        {repartition[3]}
+      </CustomText>
+    </mesh>
+  );
+};
+
+interface CustomCubeProps {
+  cities?: ICity[];
+}
+
+const Cube: React.FunctionComponent<CustomCubeProps> = ({ cities = [] }) => {
+  const myCities = cities
+    .filter((city) => city.mine)
+    .map((city) => city.cityName);
+  const otherCities = cities
+    .filter((city) => !city.mine)
+    .map((city) => city.cityName);
   return (
     <mesh position={[-100, 0, 0]}>
       <Box height={200} width={200} />
-      <InnerTextBox />
+      <InnerTextBox text={otherCities} />
+      <OuterTextBox text={myCities} />
     </mesh>
   );
-}
+};
 
 const ArtContainer = () => {
-  const cities: readonly ICity[] = useSelector(
+  const cities: ICity[] = useSelector(
     (state: CityState) => state.cities,
     shallowEqual
   );
-  console.log(cities);
 
   return (
     <Canvas
@@ -213,7 +252,7 @@ const ArtContainer = () => {
       <OrbitControls />
       <ambientLight intensity={0.5} />
       {/* <pointLight position={[0, 0, 0]} /> */}
-      <Cube />
+      <Cube cities={cities} />
     </Canvas>
   );
 };
---
+++ src/helpers/helperTextRepartition.tsx
@@ -0,0 +1,29 @@
+const MAX_CARAC = 1300;
+
+export const textRepartition = (text: string[], nbSide: number) => {
+  let intResult: string[][] = [];
+  let nbChara: number[] = [];
+  for (let i = 0; i < nbSide; i++) {
+    intResult.push([]);
+    nbChara.push(0);
+  }
+
+  text.forEach((text, index) => {
+    console.log(index % nbSide);
+    if (intResult[index % nbSide].length < MAX_CARAC) {
+      intResult[index % nbSide].push(text);
+      nbChara[index % nbSide] += text.length;
+    }
+  });
+  // .concat(area.join(" ‎".repeat(nb_space_arround)))
+
+  let returnArray: string[] = intResult.map((area, index) => {
+    let nb_space_arround = (MAX_CARAC - nbChara[index]) / (area.length + 2);
+    console.log(nb_space_arround);
+    return " ‎ ‎"
+      .repeat(Math.random() * nb_space_arround)
+      .concat(area.join(" ‎  ‎".repeat(nb_space_arround)))
+      .concat(" ‎ ‎".repeat(Math.random() * nb_space_arround));
+  });
+  return returnArray;
+};
